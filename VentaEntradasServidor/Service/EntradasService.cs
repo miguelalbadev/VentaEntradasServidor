b8c748abb75b28dc22b2118da9dabf28c12bcc6b
usuario <usuario@L-1706039>
2017-08-29 18:32:50 +0200

    Segundo commit

---
+++ VentaEntradasServidor/Service/EntradasService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaEntradasServidor.Models;
+using VentaEntradasServidor.Repository;
+
+namespace VentaEntradasServidor.Service {
+    public class EntradasService : IEntradasService {
+        private IEntradasRepository entradasRepository;
+
+        public EntradasService(IEntradasRepository entradasRepository) {
+            this.entradasRepository = entradasRepository;
+        }
+
+        public Entrada Create(Entrada entrada) {
+            return entradasRepository.Create(entrada);
+        }
+
+        public Entrada Delete(long id) {
+            return entradasRepository.Delete(id);
+        }
+
+        public Entrada Read(long id) {
+            return entradasRepository.Read(id);
+        }
+
+        public IQueryable<Entrada> ReadAll() {
+            throw new NotImplementedException();
+        }
+
+        public void Update(long id, Entrada entrada) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VentaEntradasServidor/Service/IEntradasService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VentaEntradasServidor.Models;
+
+namespace VentaEntradasServidor.Service {
+    public interface IEntradasService {
+        Entrada Create(Entrada entrada);
+        Entrada Read(long Id);
+        IQueryable<Entrada> ReadAll();
+        void Update(long id, Entrada entrada);
+        Entrada Delete(long id);
+    }
+}
